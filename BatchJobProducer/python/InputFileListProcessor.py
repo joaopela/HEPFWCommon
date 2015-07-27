bbb10aa5ea5feebc052fb9141e0fb34389f30380
Joao Pela <pela@cern.ch>
2015-07-27 20:44:18 +0200

    New spliting by number of jobs and number of files per job

--- BatchJobProducer/python/InputFileListProcessor.py
+++ BatchJobProducer/python/InputFileListProcessor.py
@@ -1,7 +1,13 @@
 class InputFileListProcessor:
 
   def __init__(self):
+    
     self.inputFiles = list()
+    self.outputDir  = ""
+    self.nJobs      = 1
+    self.listFormat = 'cmssw'
+    self.nFiles     = 0
+    
   
   def getFromDAS(self,dataset):
 
@@ -23,10 +29,14 @@ class InputFileListProcessor:
     errF.seek(0) # rewind to the beginning of the file
     
     self.inputFiles = outF.read().splitlines()
+    self.nFiles     = len(self.inputFiles)
 
-  def dump_CMSSW_cfi(self,fileName):
+    outF.close()
+    errF.close()
+
+  def dump_CMSSW_cfi(self,fileName,filelist):
     
-    outFile = open(fileName,'w');
+    outFile = open(self.outputDir+"/"+fileName,'w');
 
     outFile.write("import FWCore.ParameterSet.Config as cms\n");
     outFile.write("\n")
@@ -38,13 +48,15 @@ class InputFileListProcessor:
     
     count=1
     
-    for f in self.inputFiles:
-      outFile.write("       '"+f);
+    for f in filelist:
+      outFile.write("       '"+f+"'");
       
       if count % 254==0:
         outFile.write("\n");
         outFile.write("       ] );\n");
         outFile.write("readFiles.extend( [\n");
+      elif count==len(filelist):
+        outFile.write("\n");
       else:
         outFile.write(",\n");
       
@@ -52,3 +64,36 @@ class InputFileListProcessor:
     
     outFile.write("       ] );\n");
     outFile.write("secFiles.extend([])\n");
+
+  def splitByNumberOfJobs(self,nJobs):
+
+    if self.nFiles<nJobs:
+      print "Number of jobs is bigger than number of input files... setting number of jobs to number of files"
+      nJobs=self.nFiles
+
+    filesPerJob = float(self.nFiles) / float(nJobs)
+    print "Making Jobs "+str(nJobs)+" ("+str(filesPerJob)+" files per job)"
+
+    fileIndex = 0
+    for x in range(0,nJobs):
+
+      jobInputFiles = list()
+
+      while fileIndex < (x+1)*filesPerJob:
+        jobInputFiles.append(self.inputFiles[fileIndex])
+        fileIndex=fileIndex+1
+
+      self.dump_CMSSW_cfi("inputSrc_job"+str(x+1)+"_cfi.py",jobInputFiles)
+
+
+  def splitByNumberOfFiles(self,filesPerJob):
+    
+    if self.listFormat=='cmssw':
+      
+      jobs=[self.inputFiles[x:x+filesPerJob] for x in xrange(0, len(self.inputFiles), filesPerJob)]
+      
+      jobNumber=1;
+      for j in jobs:
+        self.dump_CMSSW_cfi("inputSrc_job"+str(jobNumber)+"_cfi.py",j)
+        jobNumber=jobNumber+1
+
